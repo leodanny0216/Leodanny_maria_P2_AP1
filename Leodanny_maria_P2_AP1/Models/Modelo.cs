7c7d8b5bd4e8279c02c18228b69c4552fe16be32
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-03-10 17:14:29 -0400

    cree  la carpeta Models

---
+++ Leodanny_maria_P2_AP1/Models/Modelo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leodanny_maria_P2_AP1.Models
+{
+    public class Modelo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
