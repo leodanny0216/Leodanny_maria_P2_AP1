8d474d3858de32e2216dd099903c6f5a6f6fb1fc
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-03-10 17:15:20 -0400

    cree el  contexto

---
+++ Leodanny_maria_P2_AP1/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Leodanny_maria_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Leodanny_maria_P2_AP1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Modelo> Modelo { get; set; }
+    }
+}
