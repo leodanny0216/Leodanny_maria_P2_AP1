356f5a0c90357211e22b8f45139c7e16500725b1
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-03-11 00:26:03 -0400

    cree la clases encuesta del modelo

---
+++ Leodanny_maria_P2_AP1/Models/Encuesta.cs
@@ -0,0 +1,29 @@
+﻿using Leodanny_maria_P2_AP1.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Leodanny_maria_P2_AP1.Models
+{
+    public class Encuestas
+    {
+
+        [Key]
+        public int EncuestaId { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+
+        [Required(ErrorMessage = "LA asignatura debe ser obligatoria")]
+        public string Asignatura { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese el monto")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números enteros o decimales")]
+        public double Monto { get; set; }
+
+
+        [ForeignKey("EncuestaId")]
+        public ICollection<EncuestasDetalle> encuestasDetalle { get; set; } = new List<EncuestasDetalle>();
+
+
+    }
+}
